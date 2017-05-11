2f0a1aad5f8e62757a97d144dbc9fcec0b0385bb
Abel L. MBULA <bam92@debian>
2017-05-11 07:24:23 +0200

    Implementation of MVC architecture

--- index.php
+++ index.php
@@ -1,26 +1,4 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <link href="style.css" rel="stylesheet" />
-    <title>QuoteDico - Home</title>
-</head>
-<body>
-    <header>
-        <h1>QuoteDico</h1>
-    </header>
     <?php
-    $bdd = new PDO('mysql:host=localhost;dbname=dictionnary;charset=utf8', 'test', 'test');
-    $quotes = $bdd->query('select * from t_quote order by q_id desc');
-    foreach ($quotes as $quote): ?>
-    <article>
-        <p><blockquote><?php echo $quote['q_description'] ?><br />
-            <small><?php echo $quote['q_author'] ?></small></blockquote>
-        </p>
-    </article>
-    <?php endforeach ?>
-    <footer class="footer">
-        <a href="#">QuoteDico</a> is a quote dictionnary app built modern PHP development.
-    </footer>
-</body>
-</html>
+    require 'model.php';
+    $quotes = getQuotes();
+    require 'view.php';
